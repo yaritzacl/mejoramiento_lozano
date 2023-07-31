6b4123f443ba4d8da710345511c0dbcbcfb26081
yaritzacl <105559982+yaritzacl@users.noreply.github.com>
2023-07-31 10:40:33 -0500

    Update lista1.py

--- condicionales/Miselaneas/listas/lista1.py
+++ condicionales/Miselaneas/listas/lista1.py
@@ -1,20 +1,11 @@
-def generar_serie_fibonacci_digitos(n):
-    if n <= 0:
-        return []
+def seriefibo(numero):
+    fibo=[]
+    a,b=0,1
+    while numero>0:
+        fibo.append(a)
+        a,b=b,a+b
+        numero-=1
+        print (fibo)
     
-    fibonacci = [0, 10]
-
-    num = fibonacci[-1] + fibonacci[-2]
-    if (int(num)) == n:
-            fibonacci.append(num)
-         
-     
-
-    return fibonacci
-
-
-n = int(input("Ingrese la cantidad de dígitos de la serie de Fibonacci que desea generar: "))
-fibonacci_series = generar_serie_fibonacci_digitos(n)
-print("Serie de Fibonacci generada:")
-print(fibonacci_series)
+t=seriefibo(numero=int(input("ingrese el numero:")))
 
