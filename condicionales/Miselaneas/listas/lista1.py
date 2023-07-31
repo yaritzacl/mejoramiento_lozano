c72d72137c30b5b4574cfa7347b9351bbe9cebd9
mejoramiento_lozano <zully9888@gmail.com>
2023-07-30 19:00:46 -0500

    mejoramiento

---
+++ condicionales/Miselaneas/listas/lista1.py
@@ -0,0 +1,20 @@
+def generar_serie_fibonacci_digitos(n):
+    if n <= 0:
+        return []
+    
+    fibonacci = [0, 10]
+
+    num = fibonacci[-1] + fibonacci[-2]
+    if (int(num)) == n:
+            fibonacci.append(num)
+         
+     
+
+    return fibonacci
+
+
+n = int(input("Ingrese la cantidad de dígitos de la serie de Fibonacci que desea generar: "))
+fibonacci_series = generar_serie_fibonacci_digitos(n)
+print("Serie de Fibonacci generada:")
+print(fibonacci_series)
+
